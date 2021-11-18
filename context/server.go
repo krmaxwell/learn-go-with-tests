342705f12edcd5baaee6ba8c1be543d7abaa6672
Kyle Maxwell <kylemxl@icloud.com>
2021-11-17 18:51:59 -0600

    Implement nicer handling

--- context/server.go
+++ context/server.go
@@ -1,29 +1,23 @@
 package store
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		data := make(chan string, 1)
+		data, err := store.Fetch(r.Context())
 
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(rw, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return // todo: log this error somehow
 		}
+
+		fmt.Fprint(rw, data)
 	}
 }

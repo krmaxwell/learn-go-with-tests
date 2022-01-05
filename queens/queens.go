10471aa1a00770b99c57ee8ab06649948efd2404
Kyle Maxwell <kylemxl@icloud.com>
2022-01-04 18:15:33 -0600

    More validation

--- queens/queens.go
+++ queens/queens.go
@@ -9,3 +9,35 @@ func CreateBoard(order int) Board {
 	}
 	return board
 }
+
+func ValidateBoard(b Board) bool {
+	// returns true if board has one true value on each row, column, and diagonal
+
+	for _, row := range b {
+		if !validateSlice(row) {
+			return false
+		}
+	}
+
+	for col := range b[0] {
+		colSlice := make([]bool, len(b[0]))
+		for row := range b {
+			colSlice = append(colSlice, b[row][col])
+		}
+		if !validateSlice(colSlice) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func validateSlice(s []bool) bool {
+	count := 0
+	for _, cell := range s {
+		if cell {
+			count++
+		}
+	}
+	return count == 1
+}
--- queens/queens_test.go
+++ queens/queens_test.go
@@ -17,3 +17,41 @@ func TestBoardCreation(t *testing.T) {
 	assert.IsType(t, Board{}, board)
 	assert.False(t, board[0][0])
 }
+
+func TestValidation(t *testing.T) {
+	validBoard := Board{
+		{true, false, false},
+		{false, false, true},
+		{false, true, false},
+	}
+	assert.True(t, ValidateBoard(validBoard))
+
+	allFalseBoard := Board{
+		{false, false, false},
+		{false, false, false},
+		{false, false, false},
+	}
+	assert.False(t, ValidateBoard(allFalseBoard))
+
+	allTrueBoard := Board{
+		{true, true, true},
+		{true, true, true},
+		{true, true, true},
+	}
+	assert.False(t, ValidateBoard(allTrueBoard))
+
+	firstColumnBoard := Board{
+		{true, false, false},
+		{true, false, false},
+		{true, false, false},
+	}
+	assert.False(t, ValidateBoard(firstColumnBoard))
+
+	firstRowBoard := Board{
+		{true, true, true},
+		{false, false, false},
+		{false, false, false},
+	}
+	assert.False(t, ValidateBoard(firstRowBoard))
+
+}
